8c19380702cd369bfd61ca8c3fcbaf8a31eebe8c
onoyamayoichi <yonoyama@onocomm.co.jp>
2025-03-18 13:56:36 +0900

    Refactor comments for clarity and update IAM role policies in EC2 ImageBuilder stack

--- lib/cdk-ec2-imagebuilder-stack.ts
+++ lib/cdk-ec2-imagebuilder-stack.ts
@@ -24,10 +24,10 @@ export class CdkEc2ImageBuilderStack extends Stack {
     
     // ----------コンポーネント設定----------
 
-    // コンポーネント定義ファイルを読み込む
+    // ✅ コンポーネント定義ファイルを読み込む
     const componentData = fs.readFileSync(path.join(__dirname, '../components/ec2-component.txt'), 'utf8');
 
-    // ImageBuilder用のコンポーネントを作成
+    // ✅ ImageBuilder用のコンポーネントを作成
     const component = new imagebuilder.CfnComponent(this, 'InstallComponent', {
       name: `${ResourceName}Component`,
       platform: 'Linux',
@@ -37,15 +37,25 @@ export class CdkEc2ImageBuilderStack extends Stack {
 
     // ----------インフラストラクチャー設定----------
 
-    // ImageBuilder用のIAMロールを作成
+    // ✅ ImageBuilder用のIAMロールを作成
     const imageBuilderRole = new iam.Role(this, 'ImageBuilderRole', {
       roleName: `${ResourceName}ImageBuilderRole`,
-      assumedBy: new iam.ServicePrincipal('imagebuilder.amazonaws.com'),
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
+        //iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('EC2InstanceProfileForImageBuilder'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMFullAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
       ],
     });
     
+    // ✅ インスタンスプロファイルの作成
+    const instanceProfile = new iam.CfnInstanceProfile(this, 'InstanceProfile', {
+      instanceProfileName: imageBuilderRole.roleName, // ✅ IAMロールと同じ名前を指定
+      roles: [imageBuilderRole.roleName],
+    });
+
     // ✅ デフォルト VPC を取得
     const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', {
       isDefault: true, // ✅ デフォルト VPC を取得
@@ -62,11 +72,6 @@ export class CdkEc2ImageBuilderStack extends Stack {
       allowAllOutbound: true,
     });
 
-    // インスタンスプロファイルの作成
-    const instanceProfile = new iam.CfnInstanceProfile(this, 'InstanceProfile', {
-      roles: [imageBuilderRole.roleName],
-    });
-
     // ✅ Image Builder のインフラ設定を作成
     const infrastructureConfiguration = new imagebuilder.CfnInfrastructureConfiguration(this, 'InfraConfig', {
       name: `${ResourceName}InfrastructureConfiguration`,
@@ -78,7 +83,7 @@ export class CdkEc2ImageBuilderStack extends Stack {
 
     // ----------レシピ設定----------
 
-    // レシピを作成
+    //  ✅ レシピを作成
     const recipe = new imagebuilder.CfnImageRecipe(this, 'Recipe', {
       name: `${ResourceName}Recipe`,
       version: '1.0.0',
@@ -110,7 +115,7 @@ export class CdkEc2ImageBuilderStack extends Stack {
 
     // ----------配布設定----------
 
-    // 配布設定
+    // ✅ 配布設定
     const distributionConfiguration = new imagebuilder.CfnDistributionConfiguration(this, 'DistributionConfig', {
       name: `${ResourceName}DistributionConfiguration`,
       distributions: [
@@ -128,13 +133,13 @@ export class CdkEc2ImageBuilderStack extends Stack {
 
     // ----------パイプライン設定----------
 
-    // イメージテスト設定
+    // ✅ イメージテスト設定
     const imageTestsConfiguration = {
       imageTestsEnabled: true,
       timeoutMinutes: 60,
     };
 
-    // イメージパイプラインを作成
+    // ✅ イメージパイプラインを作成
     const pipeline = new imagebuilder.CfnImagePipeline(this, 'Pipeline', {
       name: `${ResourceName}Pipeline`,
       infrastructureConfigurationArn: infrastructureConfiguration.attrArn,
@@ -145,7 +150,7 @@ export class CdkEc2ImageBuilderStack extends Stack {
 
     // ----------イメージ作成----------
 
-    // AMIを作成するためのイメージを作成
+    // ✅ AMIを作成するためのイメージを作成
     const image = new imagebuilder.CfnImage(this, 'Image', {
       infrastructureConfigurationArn: infrastructureConfiguration.attrArn,
       distributionConfigurationArn: distributionConfiguration.attrArn,
